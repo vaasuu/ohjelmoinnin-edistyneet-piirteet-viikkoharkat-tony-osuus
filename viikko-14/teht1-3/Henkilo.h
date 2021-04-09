b4ed79f50f1f2a4785ca74a31dd5f241e387da66
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-04-09 14:36:50 +0300

    Same as last week

---
+++ viikko-14/teht1-3/Henkilo.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Osoite.h"
+using namespace std;
+
+// Henkilö-luokan esittely (kirjoitetaan omaan .h tiedostoon)
+// Esittely == declaration, API
+class Henkilo {
+    // Luokan julkinen rajapinta (API), toiminnot, metodit
+    public:
+        Henkilo();
+        Henkilo(string aNimi, int aIka);
+        Henkilo(string aNimi, int aIka, Osoite aOsoite);
+        Henkilo(const Henkilo& aHenkilo); // kopiorakentaja
+        ~Henkilo();
+
+        void setNimi(string aNimi);
+        void setIka(int aIka);
+        string getNimi() const;
+        int getIka() const;
+        void setOsoite(Osoite aOsoite);
+        Osoite getOsoite() const;
+        void tulostaHenkilonTiedot() const;
+        void kysyTiedot();
+
+    // Luokan tietojäsenet (yleensä private)
+    // Näihin päästään käsiksi vain luokan metodeissa
+    private:
+        string mNimi;
+        int mIka;
+        Osoite osoite;
+};
---
+++ viikko-14/teht1-3/Osoite.cpp
@@ -0,0 +1,44 @@
+#include "Osoite.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Osoite::Osoite() : mKatuosoite("N/A"), mPostinumero("N/A"), mKunta("N/A")
+{
+    cout << "Osoite -luokan oletusrakentaja (parametriton rakentaja)" << endl;
+}
+
+Osoite::Osoite(string aKatuosoite, string aPostinumero, string aKunta)
+    : mKatuosoite(aKatuosoite), mPostinumero(aPostinumero), mKunta(aKunta)
+{
+    cout << "Osoite -luokan 3 parametrinen rakentaja" << endl;
+}
+
+Osoite::~Osoite()
+{
+    cout << "Osoite -luokan purkaja" << endl;
+}
+
+std::string Osoite::getKatuosoite() const {
+    return mKatuosoite;
+}
+std::string Osoite::getPostinumero() const {
+    return mPostinumero;
+}
+std::string Osoite::getKunta() const {
+    return mKunta;
+}
+void Osoite::setKatuosoite(std::string aKatuosoite) {
+    mKatuosoite = aKatuosoite;
+}
+void Osoite::setPostinumero(std::string aPostinumero) {
+    mPostinumero = aPostinumero;
+}
+void Osoite::setKunta(std::string aKunta) {
+    mKunta = aKunta;
+}
+void Osoite::tulostaTiedot() const {
+    cout << "Katuosoite: " << mKatuosoite << endl;
+    cout << "Postinumero: " << mPostinumero << endl;
+    cout << "Kunta: " << mKunta << endl;
+}
