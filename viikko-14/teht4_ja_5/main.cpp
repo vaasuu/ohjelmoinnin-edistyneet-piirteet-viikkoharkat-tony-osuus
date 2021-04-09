9b61a730239e9760ebdef49e1c11f780c56d1da3
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-04-10 00:39:59 +0300

    Viikko 14 teht 5 done

--- viikko-14/teht4_ja_5/main.cpp
+++ viikko-14/teht4_ja_5/main.cpp
@@ -2,10 +2,12 @@
 #include "Opiskelija.h"
 #include "Opettaja.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
 
+    // teht 4
     cout << "––––––––––––––––––––––––––––––––––––––––––––––––––" << endl;
     
     Henkilo a;
@@ -26,5 +28,20 @@ int main() {
     
     cout << "––––––––––––––––––––––––––––––––––––––––––––––––––" << endl;
 
+    // teht 5
+
+    vector <Henkilo*> henkilo_ptr_vektori;
+
+    henkilo_ptr_vektori.push_back(new Henkilo);
+    henkilo_ptr_vektori.push_back(new Opettaja);
+    henkilo_ptr_vektori.push_back(new Opiskelija);
+
+    // Vektorin kautta voi kutsua vain Henkilö-luokan metodeja, koska ne ovat Henkilö-tyyppisiä.
+
+    for (Henkilo* x : henkilo_ptr_vektori)
+    {
+        x->tulostaHenkilonTiedot();
+    }
+    
     return 0;
 }
