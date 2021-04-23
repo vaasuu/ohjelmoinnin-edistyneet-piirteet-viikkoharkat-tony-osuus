21d5ed8cccaa082d2824e6bd237920b29b08f366
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-04-23 19:52:37 +0300

    Same as 2 weeks ago

---
+++ viikko-16/teht1/Opettaja.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Henkilo.h"
+using namespace std;
+
+class Opettaja : public Henkilo
+{
+public:
+    Opettaja(); // parametriton oletusrakentaja
+    Opettaja(const string& aNimi, const int& aIka, const Osoite& aOsoite, const string& aKoulutusala); // 4 parametrinen oletusrakentaja
+    Opettaja(const Opettaja& aOpettaja); // kopiorakentaja
+    ~Opettaja(); // purkaja
+    
+    void setKoulutusala(const string& aKoulutusala);
+    string getKoulutusala() const;
+    
+    void setVastuukurssit(const vector <string>& aVastuukurssit);
+    vector <string> getVastuukurssit() const; 
+    void lisaaKurssi(string aKurssi);
+    void poistaKurssi(string aKurssi);
+    void tulostaTiedot();
+
+private:
+    string Koulutusala;
+    vector <string> Vastuukurssit;
+};
---
+++ viikko-16/teht1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Opiskelija.h"
+#include "Opettaja.h"
+using namespace std;
+
+int main() {
+    return 0;
+}
