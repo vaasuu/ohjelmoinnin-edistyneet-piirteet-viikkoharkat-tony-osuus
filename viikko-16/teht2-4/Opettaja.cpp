50799bb69d87039f607d166886a5cf972d19cf9e
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2021-04-23 20:46:53 +0300

    Same as 2 weeks ago

---
+++ viikko-16/teht2-4/Opettaja.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "Opettaja.h"
+#include "Henkilo.h"
+#include "Osoite.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+Opettaja::Opettaja() // parametriton rakentaja 
+{
+    cout << "Opettaja parametriton rakentaja" << endl;
+} 
+
+Opettaja::Opettaja(const string& aNimi, const int& aIka, const Osoite& aOsoite, const string& aKoulutusala) // 4 parametrinen oletusrakentaja
+: Henkilo(aNimi, aIka, aOsoite), Koulutusala(aKoulutusala) 
+{
+    cout << "Opettaja 4 parametrinen oletusrakentaja" << endl;
+} 
+
+Opettaja::Opettaja(const Opettaja& aOpettaja) // kopiorakentaja
+    : Henkilo(aOpettaja.getNimi(), aOpettaja.getIka(), Osoite(aOpettaja.getOsoite())), Koulutusala(aOpettaja.Koulutusala), Vastuukurssit(aOpettaja.Vastuukurssit)
+{
+    cout << "Opettaja kopiorakentaja" << endl;
+}
+
+Opettaja::~Opettaja() { // purkaja
+    cout << "Opettaja purkaja" << endl;
+} 
+
+void Opettaja::setKoulutusala(const string& aKoulutusala) {
+    Koulutusala = aKoulutusala;
+}
+string Opettaja::getKoulutusala() const {
+    return Koulutusala;
+}
+
+void Opettaja::setVastuukurssit(const vector <string>& aVastuukurssit) {
+    Vastuukurssit = aVastuukurssit;
+}
+
+vector <string> Opettaja::getVastuukurssit() const {
+    return Vastuukurssit;
+} 
+
+void Opettaja::lisaaKurssi(string aKurssi) {
+    Vastuukurssit.push_back(aKurssi);
+}
+
+void Opettaja::poistaKurssi(string aKurssi) {
+    bool deleted = false;
+    for (size_t i = 0; i < Vastuukurssit.size(); i++)
+    {
+        if (Vastuukurssit[i] == aKurssi)
+        {
+            Vastuukurssit.erase(Vastuukurssit.begin()+i);
+            cout << aKurssi << " on nyt poistettu!" << endl;
+            deleted = true;
+        }
+        
+    }
+    if (!deleted)
+    {
+        cout << "Ei löydy kurssia nimeltä \"" << aKurssi << "\"" << endl;
+    }
+}
+
+void Opettaja::tulostaTiedot() {
+    tulostaHenkilonTiedot();
+    cout << "Koulutusala: " << Koulutusala << endl;
+    cout << "Vastuukurssit: " << endl;
+    for (string kurssi : Vastuukurssit)
+    {
+        cout << " " << kurssi << endl;
+    }
+}
---
+++ viikko-16/teht2-4/Opiskelija.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Henkilo.h"
+using namespace std;
+
+class Opiskelija : public Henkilo
+{
+public:
+    Opiskelija(); // parametriton oletusrakentaja
+    Opiskelija(const string& aNimi, const int& aIka, const Osoite& aOsoite, const string& aOpNro, const int& aOpintopisteet); // 4 parametrinen oletusrakentaja
+    Opiskelija(const Opiskelija& aOpiskelija); // kopiorakentaja
+    ~Opiskelija(); // purkaja
+    
+    void setOpNro(const string& aOpNro);
+    string getOpNro() const;
+    
+    void setSuoritetutKurssit(const vector <string>& aSuoritetutKurssit);
+    vector <string> getSuoritetutKurssit() const; 
+    void lisaaKurssi(string aKurssi);
+    
+    void tulostaTiedot();
+    
+    void setOpintopisteet(const int& aOpintopisteet);
+    int getOpintopisteet() const;
+    void lisaaOpintopisteita(const int& aOpintopisteet);
+
+private:
+    string opNro;
+    vector <string> suoritetutKurssit;
+    int opintopisteet;
+};
